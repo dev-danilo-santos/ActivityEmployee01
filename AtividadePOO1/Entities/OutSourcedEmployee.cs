b3a72bf0f772b62f029329e80d620948b009059a
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-03-13 19:22:53 -0300

    Atividade completa e funcionando

---
+++ AtividadePOO1/Entities/OutSourcedEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePOO1.Entities
+{
+    sealed class OutSourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; private set; }
+
+        public OutSourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) 
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            double total;
+            total = base.Payment();
+            return total + (AdditionalCharge*1.1);
+        }
+
+
+
+
+    }
+}
---
+++ AtividadePOO1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AtividadePOO1.Entities;
+
+namespace AtividadePOO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Employee {i + 1} data: ");
+                Console.Write("Outsourced? (y/n) ");
+                string o = Console.ReadLine().Trim().ToLower();
+
+                if (o.Equals("n"))
+                {
+                    Console.WriteLine("Name: ");
+                    string Name = Console.ReadLine();
+                    Console.WriteLine("Hours: ");
+                    int Hours = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Value per hour: ");
+                    double Value = double.Parse(Console.ReadLine());
+                    Employee Employee = new Employee(Name, Hours, Value);
+                    employees.Add(Employee);
+                }
+                else if (o.Equals("y"))
+                {
+                    Console.WriteLine("Name: ");
+                    string Name = Console.ReadLine();
+                    Console.WriteLine("Hours: ");
+                    int Hours = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Value per hour: ");
+                    double Value = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Additional Charge: ");
+                    double AdditionalCharge = double.Parse(Console.ReadLine());
+                    Employee Employee = new OutSourcedEmployee(Name, Hours, Value, AdditionalCharge);
+                    employees.Add(Employee);
+                }
+            }
+
+            Console.WriteLine("Payments: ");
+            StringBuilder sb = new StringBuilder();
+            foreach (Employee employee in employees)
+            {
+                sb.AppendLine($"{employee.Name} - $ {employee.Payment()}");    
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
